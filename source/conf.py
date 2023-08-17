3cbfe75a12d360b162bbb2aaa5f7b3e6aab31770
eviau <info@eviau.net>
2023-08-16 23:42:05 -0400

    init sphinx and sphinx-intl

---
+++ source/conf.py
@@ -0,0 +1,26 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Best practices for conservation of media art from an artist's perspective"
+copyright = '2023, Rafael Lozano-Hemmer'
+author = 'Rafael Lozano-Hemmer'
+release = '0.9.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,20 @@
+.. Best practices for conservation of media art from an artist's perspective documentation master file, created by
+   sphinx-quickstart on Wed Aug 16 22:53:22 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Best practices for conservation of media art from an artist's perspective's documentation!
+=====================================================================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
